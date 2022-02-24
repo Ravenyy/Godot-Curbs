2e9b6e65b054a772dee2dbd34da34e3c5c24bc48
Ravenyy <35702960+Ravenyy@users.noreply.github.com>
2022-02-24 18:09:15 +0100

    Project restructurisation

--- scripts/Throw.gd
+++ scripts/Throw.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 onready var visibility_notifier = get_node("VisibilityNotifier2D")
 var dragging
 var drag_start = Vector2()
-
+signal touch
 
 func _input(event):
 	if event.is_action_pressed("click") and not dragging:
@@ -20,3 +20,8 @@ func _input(event):
 func _on_VisibilityNotifier2D_screen_exited():
 	mode = MODE_STATIC
 	set_position(Vector2(154, 377))
+
+
+func _on_Ball_body_entered(body):
+	emit_signal("touch")
+	print("chuj")
